661658a85ede2182b94ad2994494d66cc323d207
Tarek Ayoubi <tarek20501@hotmail.com>
2020-10-08 00:15:07 -0700

    Add better Time constructor Add use cases in main

--- src/main/model/Time.java
+++ src/main/model/Time.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Time {
     public int hours;
     public int minutes;
 
+    // REQUIRES: time to be in "HH:MM" format.
+    public Time(String time) {
+        String[] hoursMinutes;
+        hoursMinutes = time.split(":", 2);
+        this.hours = Integer.parseInt(hoursMinutes[0]);
+        this.minutes = Integer.parseInt(hoursMinutes[1]);
+    }
+
+    //REQUIRES: hours in [0,23] and minutes in [0,59]
     public Time(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
--- src/main/ui/WeekPlannerCLI.java
+++ src/main/ui/WeekPlannerCLI.java
@@ -1,10 +1,26 @@
 package ui;
 
+import model.Day;
 import model.Time;
 import model.TimeBlock;
 
 public class WeekPlannerCLI {
     public static void main(String[] args) {
         System.out.println("Hello WeekPlanner");
+
+        Day sat = new Day("Saturday");
+        Day sun = new Day("Sunday");
+
+        TimeBlock cpsc121 = new TimeBlock(new Time("09:30"), new Time("11:00"));
+        TimeBlock cpsc210 = new TimeBlock(new Time("11:00"), new Time("12:00"));
+
+        sat.addTimeBlock(cpsc121);
+        sun.addTimeBlock(cpsc210);
+
+        sat.deleteTimeBlock(cpsc121);
+
+        sun.resizeTimeBlock(cpsc210, new Time("10:00"), new Time("13:00"));
+
+        sun.moveTimeBlock(cpsc210, sat, new Time("10:00"), new Time("12:00"));
     }
 }
