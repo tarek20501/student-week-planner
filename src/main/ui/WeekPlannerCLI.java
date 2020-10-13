bbfe70c13695588ce595387dbd4e42a3f446939a
Tarek Ayoubi <tarek20501@hotmail.com>
2020-10-12 18:41:14 -0700

    Document WeekPlannerCLI methods

--- src/main/ui/WeekPlannerCLI.java
+++ src/main/ui/WeekPlannerCLI.java
@@ -13,6 +13,10 @@ public class WeekPlannerCLI {
     private boolean stillRunning;
     private Scanner userInput;
 
+    // EFFECTS: Instantiate a list to hold the days of the week.
+    // Adds the 7 days of the week to that list.
+    // Instantiate a scanner to get user input from CLI.
+    // Runs the application
     public WeekPlannerCLI() {
         week = new ArrayList<>();
         week.add(new Day("Monday"));
@@ -29,6 +33,7 @@ public class WeekPlannerCLI {
         run();
     }
 
+    // EFFECTS: Takes user's commands and calls the function that executes them.
     private void run() {
         while (stillRunning) {
             System.out.print("Enter the character of what you want to do: ");
@@ -55,6 +60,10 @@ public class WeekPlannerCLI {
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: Lets the user select which day to add a time block to.
+    // Lets the user enter the time block.
+    // Adds the time block to the week.
     private void addTimeBlock() {
         Day dayChosen = getDayFromUser("to add to");
         TimeBlock timeBlock = getTimeBlockFromUser("add");
@@ -65,6 +74,11 @@ public class WeekPlannerCLI {
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: Lets the user select which day to modify a time block from.
+    // Lets the user select which time block to modify.
+    // Lets the user enter the time block's new settings.
+    // Applies the new settings to the time block.
     private void modifyTimeBlock() {
         showTimeBlocksInWeek();
         Day chosenDay = getDayFromUser("to modify from");
@@ -88,6 +102,10 @@ public class WeekPlannerCLI {
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: Lets the user select which day to delete a time block from.
+    // Lets the user choose which time block to delete.
+    // Deletes the selected time block.
     private void deleteTimeBlock() {
         showTimeBlocksInWeek();
         Day chosenDay = getDayFromUser("to delete from");
@@ -104,6 +122,8 @@ public class WeekPlannerCLI {
         }
     }
 
+    // EFFECTS: Lets the user select a day of the week.
+    // Returns a reference to the day chosen.
     private Day getDayFromUser(String purpose) {
         System.out.println("0.Mon 1.Tue 2.Wed 3.Thu 4.Fri 5.Sat 6.Sun");
         int choice = getIntInRangeFromUser(
@@ -113,6 +133,9 @@ public class WeekPlannerCLI {
         return week.get(choice);
     }
 
+    // EFFECTS: Lets user enter the label, start and end times of a time block.
+    // Instantiates a time block object with the attributes defined by the user.
+    // Returns a reference to it.
     private TimeBlock getTimeBlockFromUser(String purpose) {
         System.out.print("Enter the label of the time block to " + purpose + ": ");
         String label = userInput.next();
@@ -123,39 +146,23 @@ public class WeekPlannerCLI {
         return timeBlock;
     }
 
-    private void showTimeBlocksInWeek() {
-        for (Day day : week) {
-            System.out.println(day.getLabel());
-            for (TimeBlock timeBlock : day.getTimeBlocks()) {
-                System.out.print("\t");
-                printTimeBlock(timeBlock);
+    // EFFECTS: Lets the user enter time as a string "HH:MM".
+    // Instantiates a LocalTime object from that string abd returns a reference to it.
+    private LocalTime getTimeFromUser(String prompt) {
+        while (true) {
+            System.out.print(prompt + ": ");
+            String timeStr = userInput.next();
+            try {
+                return LocalTime.parse(timeStr);
+            } catch (Exception e) {
+                System.out.println("ERROR: You must enter in this format HH:MM (HH:00-23, MM:00-59)");
             }
         }
     }
 
-    private void showTimeBlocksInDay(Day day) {
-        int i = 0;
-        for (TimeBlock timeBlock : day.getTimeBlocks()) {
-            System.out.print(i + ". ");
-            printTimeBlock(timeBlock);
-            i++;
-        }
-    }
-
-    private void printTimeBlock(TimeBlock timeBlock) {
-        System.out.print(timeBlock.getLabel() + ": ");
-        System.out.print(timeBlock.getStartTime().toString() + " - ");
-        System.out.println(timeBlock.getEndTime().toString());
-    }
-
-    private void printHelp() {
-        System.out.println("s: Show time blocks in your week");
-        System.out.println("a: Add a time block to your week");
-        System.out.println("m: Modify a time block in your week");
-        System.out.println("d: Delete a time block in your week");
-        System.out.println("x: Exit the program");
-    }
-
+    // EFFECTS: Takes an integer from the user and verifies that it falls in range
+    // between start and end inclusive. Keeps asking the user for an input until
+    // a valid one is received which is then returned.
     private int getIntInRangeFromUser(String prompt, int start, int end) {
         while (true) {
             System.out.print(prompt + ": ");
@@ -173,15 +180,41 @@ public class WeekPlannerCLI {
         }
     }
 
-    private LocalTime getTimeFromUser(String prompt) {
-        while (true) {
-            System.out.print(prompt + ": ");
-            String timeStr = userInput.next();
-            try {
-                return LocalTime.parse(timeStr);
-            } catch (Exception e) {
-                System.out.println("ERROR: You must enter in this format HH:MM (HH:00-23, MM:00-59)");
+    // EFFECTS: Prints all days of the week, and under each day, time blocks on that
+    //  day are printed too.
+    private void showTimeBlocksInWeek() {
+        for (Day day : week) {
+            System.out.println(day.getLabel());
+            for (TimeBlock timeBlock : day.getTimeBlocks()) {
+                System.out.print("\t");
+                printTimeBlock(timeBlock);
             }
         }
+    }
+
+    // EFFECTS: Prints time blocks in the given day as a numbered list.
+    private void showTimeBlocksInDay(Day day) {
+        int i = 0;
+        for (TimeBlock timeBlock : day.getTimeBlocks()) {
+            System.out.print(i + ". ");
+            printTimeBlock(timeBlock);
+            i++;
+        }
+    }
+
+    // EFFECTS: Prints timeBlock's label, start and end times.
+    private void printTimeBlock(TimeBlock timeBlock) {
+        System.out.print(timeBlock.getLabel() + ": ");
+        System.out.print(timeBlock.getStartTime().toString() + " - ");
+        System.out.println(timeBlock.getEndTime().toString());
+    }
+
+    // EFFECTS: Prints help message showing available commands and what they do.
+    private void printHelp() {
+        System.out.println("s: Show time blocks in your week");
+        System.out.println("a: Add a time block to your week");
+        System.out.println("m: Modify a time block in your week");
+        System.out.println("d: Delete a time block in your week");
+        System.out.println("x: Exit the program");
     }
 }
